763ce94c1e746b731b6e4ddbe47f49d256907652
Mary1812 <mariyapiligrim3@gmail.com>
2021-10-18 16:16:16 +0300

    Add Counter on redux

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
-import { useReducer } from 'react';
+import { connect } from 'react-redux';
 
-const initialState = {
-  count: 0
-}
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment': {
-      const newState = {
-        ...state,
-        count: state.count +1 
-      }
-      return newState;
-    }
-    case 'decrement': {
-      const newState = {
-        ...state,
-        count: state.count -1 
-      }
-      return newState;
-}
-    default: return state;
-  }
-}
-
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState)
+function App(props) {
+  console.log(props);
+  const { count, dispatch} = props;
 
   const increment = () => {
     const action = {
       type: 'increment' 
     }
 
-    dispatch(action);
+    dispatch(action)
   }
 
   const decrement = () => {
     const action = {
       type: 'decrement' 
     }
-
-    dispatch(action);
+    
+    dispatch(action)
   }
   return (
     <div>
-      <h1>Counter is: {state.count}</h1>
+      <h1>Counter is: {count} </h1>
       <button onClick={increment}> + </button>
       <button onClick={decrement}> - </button>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+const withProps = connect(mapStateToProps);
+
+const AppWithProps = withProps(App); //паттерн компонента высшего порядка
+
+export default AppWithProps;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+
+const initialState = {
+  count: 0,
+  test: true,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "increment": {
+      const newState = {
+        count: state.count + 1,
+      };
+      return newState;
+    }
+    case "decrement": {
+      const newState = {
+        count: state.count - 1,
+      };
+      return newState;
+    }
+      
+    default:
+      return state;
+  }
+}
+
+const allstore = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={allstore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*REDUX
+store - хранилище всех общих состояний приложения, объект JS
+reducer - функция, прнимающая состояние и действие и возвращающая новое состояние
+action - 
+
+*/
+
+/*REACT-REDUX
+Provider - компонент провайдер как в контексте
+connect - функция 
+*/
